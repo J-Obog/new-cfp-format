b49b1bd6542169b8ce670fad798d1eed8151b001
J-Obog <jobogbaimhe@gmail.com>
2025-12-07 23:55:35 -0500

    initial sim code

--- sim.py
+++ sim.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from common import get_soup_from_file
+import requests
 
 SCHOOL_OPTIONS_FILENAME = "files/sim/sim_school_options.html"
 
@@ -13,7 +14,15 @@ class SimResult:
 class SimUtils:
     @staticmethod
     def sim_game(year: int, team_a: str, team_b: str):
-        pass
+        payload = {
+            "HomeTeam": team_a, 
+            "HomeYear": year + 1, 
+            "AwayTeam": team_b, 
+            "AwayYear": year + 1, 
+            "hs": 1
+        }
+        data = requests.post("https://www.mygamesim.com/cfb/fb_gamesimulator.asp", data=payload)        
+        
 
     @staticmethod
     def get_sim_school_to_id_map() -> dict[SchoolName, SchoolId]:
